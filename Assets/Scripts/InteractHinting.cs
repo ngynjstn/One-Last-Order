180137d500bd142c31c7a54be74f1187bd624e6b
Jonathan Pham <jonathanvupham@gmail.com>
2025-04-13 20:49:44 -0400

    refactor 2

--- Assets/Scripts/InteractHinting.cs
+++ Assets/Scripts/InteractHinting.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHints : MonoBehaviour
 {
+    public float interactDistance;
+    public Text uiTextElement;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
